801d30eabfc8ba894132a0417ba1c5d4fd24134a
wilkin <wilkinanthony94@gmail.com>
2024-11-27 15:54:31 -0600

    week 4 final

--- movies.js
+++ movies.js
@@ -1,25 +1,22 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&s=movies
 // https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&type
+const movieListEl = document.querySelector(`.movie-list`);
 
 async function movies() {
-    const movies = await fetch('https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&s=movies')
-    const moviesData = await movies.json();
-    
-    moviesData.Search.map( 
-            (movie) => `<div class="movie__row">
-    <div class="movie__container">
-    <div class="movie__img">
-        <img src="./assets/Guardians of the Galaxy.jpg" alt="">
-    </div>
-    <p><b>Title:</b> Guardians of the Galaxy</p>
-    <p><b>Year:</b> 2017</p>
-    <p><b>Type:</b> Movie</p>
-    </div>
-</div>`
-    )
-    .join("");
-    
+  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&s=movies");
+  const moviesData = await movies.json();
+  movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
 }
 
-
 movies();
+
+function movieHTML(movie) {
+  return `<div class="movie-card">
+                  <div class="movie-card__container">
+                  <img class="movie__poster" src="${movie.Poster}" alt="">
+                    <p><b>Title:</b> ${movie.Title}</p>
+                    <p><b>Year:</b> ${movie.Year}</p>
+                    <p><b>Type:</b> ${movie.Type}</p>
+                  </div>
+                </div>`;
+}
