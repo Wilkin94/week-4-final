1e49d877d1075b49e9f2c51221866a19cc3b624d
wilkin <wilkinanthony94@gmail.com>
2024-12-21 22:55:03 -0600

    week 4 final

--- movies.js
+++ movies.js
@@ -7,19 +7,21 @@ async function getMovies() {
   const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&s=movies");
   const moviesData = await movies.json();
   movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
-  console.log(moviesData);
 }
 
-f
+
 getMovies();
 
+function showUserMovies(movie) {
+  console.log(movie);
+}
+
 function movieHTML(movie) {
-  return `<div class="movie-card" onclick>
+  return `<div class="movie-card" onclick="showUserMovies(${movie.id})">
                   <div class="movie-card__container">
                   <img class="movie__poster" src="${movie.Poster}" alt="">
-                    <p><b>Title:</b> ${movie.Title}</p>
+                    <h3><b>Title:</b> ${movie.Title}</h3>
                     <p><b>Year:</b> ${movie.Year}</p>
-                    <p><b>Type:</b> ${movie.Type}</p>
                   </div>
                 </div>`;
 }
