91403771a3d8dfbd3dd5cbbb936c8ed6d0677b2b
wilkin <wilkinanthony94@gmail.com>
2025-04-10 09:14:40 -0500

    week 4 final

---
+++ movies.js
@@ -0,0 +1,29 @@
+// https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&s=movies
+// https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&type
+const movieListEl = document.querySelector(`.movie-list`);
+let title = '';
+
+async function getMovies() {
+  const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=e5586477&s=movies");
+  const moviesData = await movies.json();
+  movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
+  console.log(moviesData);
+}
+
+getMovies();
+
+function showMoviePosts(movie) {
+  console.log(movie);
+}
+
+function movieHTML(movie) {
+  return `<div class="movie-card" onclick="showMoviePosts(movie)">
+                  <div class="movie-card__container">
+                  <img class="movie__poster" src="${movie.Poster}" alt="">
+                    <p><b>Title:</b> ${movie.Title}</p>
+                    <p><b>Year:</b> ${movie.Year}</p>
+                    <p><b>Type:</b> ${movie.Type}</p>
+                  </div>
+                </div>`;
+}
+
