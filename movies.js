644eeaf86d2afb74a53d23cc6fde6ae2fc5e1d6e
wilkin <wilkinanthony94@gmail.com>
2025-08-11 17:00:56 -0500

    week 4 final

--- movies.js
+++ movies.js
@@ -6,8 +6,19 @@ const searchInput = document.getElementById("searchInput");
 async function getMovies() {
   const movies = await fetch("https://www.omdbapi.com/?s=batman&apikey=e5586477");
   const moviesData = await movies.json();
-  movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
-  console.log(moviesData);
+  movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie) => {
+    return `
+    <div class="movie-card" onclick="">
+    <div class="movie-card__container">
+    <img class="movie__poster" src="${movie.Poster}" alt="">
+    <p><b>Title:</b> ${movie.Title}</p>
+    <p><b>Year:</b> ${movie.Year}</p>
+    <p><b>Type:</b> ${movie.Type}</p>
+    </div>
+     </div>
+     `;
+    
+  }).join("");
 }
 
 getMovies();
@@ -16,13 +27,5 @@ function onSearchResult(event) {
   console.log(event.target.value);
 }
 
-function movieHTML(movie) {
-  return `<div class="movie-card" onclick="">
-                  <div class="movie-card__container">
-                  <img class="movie__poster" src="${movie.Poster}" alt="">
-                    <p><b>Title:</b> ${movie.Title}</p>
-                    <p><b>Year:</b> ${movie.Year}</p>
-                    <p><b>Type:</b> ${movie.Type}</p>
-                  </div>
-                </div>`;
-}
+
+  
